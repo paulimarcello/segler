a4db87eef9eec43cdb32936a7c646b52433f287d
Pauli Marcello <m.pauli@softfair.de>
2019-09-30 11:32:22 +0200

    refactor

--- lib/erfassung/event.ex
+++ lib/erfassung/event.ex
@@ -1,9 +1,9 @@
 defmodule Event do
-  defstruct aggregate_id: nil,
-            occured_on: nil,
+  defstruct occured_on: nil,
+            type: nil,
             payload: nil
 
-  def new(aggregate_id, payload) do
-    %Event{aggregate_id: aggregate_id, occured_on: DateTime.utc_now(), payload: payload}
+  def new(type, payload) do
+    %Event{type: type, occured_on: DateTime.utc_now(), payload: payload}
   end
 end
--- lib/erfassung/tarifwerk.ex
+++ lib/erfassung/tarifwerk.ex
@@ -15,7 +15,7 @@ defmodule Tarifwerk do
   end
 
   def from_history(events) do
-    tarifwerk = Enum.reduce(events, new(), &apply_event({&2, &1.payload}, false))
+    tarifwerk = Enum.reduce(events, new(), &apply_event({&2, &1}, false))
     %Tarifwerk{tarifwerk | history_stream: events}
   end
 
@@ -24,27 +24,27 @@ defmodule Tarifwerk do
   end
 
   defp apply_event({tarifwerk, event}, is_new? \\ true) do
-    tarifwerk_id = tarifwerk.id
-
     tarifwerk =
       case is_new? do
         true -> %Tarifwerk{tarifwerk | new_events: [event | tarifwerk.new_events]}
         false -> tarifwerk
       end
 
-    case event do
-      {:tarifwerk_angelegt, %{tarifwerk_id: id, name: name}} ->
-        %Tarifwerk{tarifwerk | id: id, name: name}
+    case event.type do
+      :tarifwerk_angelegt ->
+        %Tarifwerk{tarifwerk | id: event.payload.tarifwerk_id, name: event.payload.name}
 
-      {:tarif_angelegt, %{tarifwerk_id: ^tarifwerk_id, tarif_id: tarif_id, tarif_name: tarif_name}} ->
-        %Tarifwerk{tarifwerk | tarife: [Tarif.new(tarif_id, tarif_name) | tarifwerk.tarife]}
+      :tarif_angelegt ->
+        neuer_tarif = Tarif.new(event.payload.tarif_id, event.payload.tarif_name)
+        %Tarifwerk{tarifwerk | tarife: [neuer_tarif | tarifwerk.tarife]}
 
-      {:tarifwerkszuschlag_angelegt, %{tarifwerk_id: ^tarifwerk_id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}} ->
-        %Tarifwerk{tarifwerk | zuschlaege: Zuschlag.add_zuschlag(tarifwerk.zuschlaege, Zuschlag.new(zuschlag_id, formel, reihenfolge))}
+      :tarifwerkszuschlag_angelegt ->
+        neuer_zuschlag = Zuschlag.new(event.payload.zuschlag_id, event.payload.formel, event.payload.reihenfolge)
+        %Tarifwerk{tarifwerk | zuschlaege: Zuschlag.add_zuschlag(tarifwerk.zuschlaege, neuer_zuschlag)}
 
-      {:tarifzuschlag_angelegt, %{tarifwerk_id: ^tarifwerk_id, tarif_id: tarif_id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}} ->
-        neuer_zuschlag = Zuschlag.new(zuschlag_id, formel, reihenfolge)
-        {[tarif | _], rest} = Enum.split_with(tarifwerk.tarife, fn t -> t.id === tarif_id end)
+      :tarifzuschlag_angelegt ->
+        neuer_zuschlag = Zuschlag.new(event.payload.zuschlag_id, event.payload.formel, event.payload.reihenfolge)
+        {[tarif | _], rest} = Enum.split_with(tarifwerk.tarife, fn t -> t.id === event.payload.tarif_id end)
         updated_tarif = Tarif.add_zuschlag(tarif, neuer_zuschlag)
         %Tarifwerk{tarifwerk | tarife: [updated_tarif | rest]}
 
@@ -55,29 +55,29 @@ defmodule Tarifwerk do
 
   def lege_neues_tarifwerk_an(%{tarifwerk_id: id, name: name}) do
     new(id, name)
-    |> create_event({:tarifwerk_angelegt, %{tarifwerk_id: id, name: name}})
+    |> create_event(:tarifwerk_angelegt, %{tarifwerk_id: id, name: name})
     |> apply_event
   end
 
   def lege_neuen_tarif_an(tarifwerk, %{tarif_id: tarif_id, name: tarif_name}) do
     tarifwerk
-    |> create_event({:tarif_angelegt, %{tarifwerk_id: tarifwerk.id, tarif_id: tarif_id, tarif_name: tarif_name}})
+    |> create_event(:tarif_angelegt, %{tarif_id: tarif_id, tarif_name: tarif_name})
     |> apply_event
   end
 
   def lege_neuen_tarifwerkszuschlag_an(tarifwerk, %{zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}) do
     tarifwerk
-    |> create_event({:tarifwerkszuschlag_angelegt, %{tarifwerk_id: tarifwerk.id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}})
+    |> create_event(:tarifwerkszuschlag_angelegt, %{zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge})
     |> apply_event
   end
 
   def lege_neuen_tarifzuschlag_an(tarifwerk, %{tarif_id: tarif_id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}) do
     tarifwerk
-    |> create_event( {:tarifzuschlag_angelegt, %{tarifwerk_id: tarifwerk.id, tarif_id: tarif_id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge}} )
+    |> create_event(:tarifzuschlag_angelegt, %{tarif_id: tarif_id, zuschlag_id: zuschlag_id, formel: formel, reihenfolge: reihenfolge})
     |> apply_event
   end
 
-  defp create_event(tarifwerk, payload) do
-    { tarifwerk, Event.new(tarifwerk.id, payload) }
+  defp create_event(tarifwerk, type, payload) do
+    { tarifwerk, Event.new(type, payload) }
   end
 end
